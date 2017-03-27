cd0d04e7d43f1971009caa64a3c787ae8115569b
Himesh Vats <icf2025317@Vats.local>
2017-03-27 16:21:35 +0530

    angularTranslate_testrun

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { TranslateModule, MissingTranslationHandler } from 'ng2-translate';
 
-import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent }   from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MyMissingTranslationHandler } from './missingtemplate.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: 'home', component: HomeComponent},
+            { path: 'about', component: AboutComponent},
+            { path: 'contact', component: ContactComponent },
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: '**', redirectTo: 'home', pathMatch: 'full' }
+        ]),
+        TranslateModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ 
+                  AppComponent,
+                  HomeComponent,
+                  AboutComponent,
+                  ContactComponent
+                ],
+  providers: [
+        { provide: MissingTranslationHandler, useClass: MyMissingTranslationHandler}
+    ],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
